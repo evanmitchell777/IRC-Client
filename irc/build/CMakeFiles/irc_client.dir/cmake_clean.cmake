9112f42dbb52fc461ba0c0dca0d696260920a248
Evan Mitchell <43766252+evanmitchell777@users.noreply.github.com>
2024-03-12 17:17:34 -0500

    Initial Push

---
+++ irc/build/CMakeFiles/irc_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/irc_client.dir/main.cpp.o"
+  "CMakeFiles/irc_client.dir/main.cpp.o.d"
+  "irc_client"
+  "irc_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/irc_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
